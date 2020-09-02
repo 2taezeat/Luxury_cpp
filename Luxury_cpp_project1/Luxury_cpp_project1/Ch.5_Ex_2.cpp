4e330cfacc4727f9f2e14dad2889e7373281e8c1
2taezeat <59787852+2taezeat@users.noreply.github.com>
2020-09-02 19:01:26 +0900

    200902 / ch.5 / 1
    200902 / ch.5 / 1

---
+++ Luxury_cpp_project1/Luxury_cpp_project1/Ch.5_Ex_2.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+void half(double &m)
+{
+    double *result;
+    result = &m;
+    *result = *result / 2;
+    
+}
+
+int main() {
+  double n = 20;
+  half(n);
+  cout << n;
+}
