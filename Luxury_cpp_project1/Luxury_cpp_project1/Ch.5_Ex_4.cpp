1a0c5c4a16accfde8df304d983934d05553f6f18
2taezeat <59787852+2taezeat@users.noreply.github.com>
2020-09-03 16:32:53 +0900

    200903 / ch.5 / 5

--- Luxury_cpp_project1/Luxury_cpp_project1/Ch.5_Ex_4.cpp
+++ Luxury_cpp_project1/Luxury_cpp_project1/Ch.5_Ex_4.cpp
@@ -1,10 +1,38 @@
 #include <iostream>
 #include <string>
+using namespace std;
+
+bool bigger(int a, int b, int& big)
+{
+	if (a == b)
+	{	
+		big = a;
+		return true;
+	}
+	else
+	{	
+		if (a > b)
+		{
+			big = a;
+		}
+		else {
+			big = b;
+		}
+		return false;
+	}
+}
 
 int main()
 {
+	int a = 3;
+	int b = 5;
+	int c = 0;
+	bool t_f;
+	cout << "두 정수를 입력하세요>> ";
+	cin >> a >> b;
 
-
-
+	t_f = bigger(a, b, c);
+	cout << "t_f>> " << t_f << endl;
+	cout << "큰 수는>> " << c;
 	return 0;
 }
