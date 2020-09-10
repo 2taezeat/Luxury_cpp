c0f7c1f7a4f6d5f7b504e6c92060d8dd87eb34ac
2taezeat <59787852+2taezeat@users.noreply.github.com>
2020-09-10 16:25:14 +0900

    200910 / ch10 / 3

--- Luxury_cpp_project1/Luxury_cpp_project1/part2/Ch.10_Ex_1.cpp
+++ Luxury_cpp_project1/Luxury_cpp_project1/part2/Ch.10_Ex_1.cpp
@@ -2,9 +2,29 @@
 #include <string>
 using namespace std;
 
+template <class T>
+T biggest(T array[], int n) {
+
+	T m = array[0];
+
+	for (int i = 0; i < n; i++)
+	{
+		if (m < array[i])
+		{
+			m = array[i];
+		}
+	}
+	return m;
+}
+
 int main() {
 
+	int x[] = { 1, 10, 100, 5, 4 };
+	cout << biggest(x, 5) << endl; // 5는 배열 x의 크기. 100이 출력된다.
 
+
+	double y[] = { 6.4, 3.3, -200.6, 7.7, 8.8, 20.3 };
+	cout << biggest(y, 6); // 6는 배열 y의 크기
 
 	return 0;
 }
